faf5c860d28bf1b574be5884bb455759a8c5964b
stefanrm@au1.ibm.com <backprop!5>
2019-05-21 15:18:19 +1000

    minor refactoring

--- nstorch/examples/count_objects.py
+++ nstorch/examples/count_objects.py
@@ -9,27 +9,22 @@ import numpy as np
 import nutsflow as nf
 import nutsml as nm
 
-from nstorch.modules import NSModule
-from nstorch.batcher import BuildBatch
-from nutsml.network import PytorchNetwork
+from nstorch.losses import mse_loss, dice_loss
+from nstorch.models import NSModule
+from nstorch.base import BuildBatch, Train, Validate, Predict
 
 
-def gen_samples(n=10, s=6):
+def gen_samples(n=100, s=6):
     for i in range(n):
         img = np.zeros((1, s, s), dtype='float32')
         img[0, 0, 0] = 1
-        img[0, 0, 2] = 1
+        img[0, 1, 1] = 1
         num = np.float32(2)
         mask = img.copy()
-        yield (img, num, 'count(filter(x))')
-        yield ([img], mask, 'filter(x)')
-
-
-def mse_loss(input, target):
-    iflat = input.view(-1)
-    tflat = target.view(-1)
-    diff = (iflat - tflat) ** 2
-    return diff.mean()
+        yield ('count(filter(_))', img, num)
+        yield ('filter(_)', [img], mask)
+        # yield ('gt(_)', 10, 1)
+        # yield ('gt(_)', 5, 0)
 
 
 def view_samples(samples):
@@ -40,6 +35,7 @@ class CountModule(nn.Module):
     def __init__(self):
         super(CountModule, self).__init__()
         self.name = 'count'
+        self.loss = mse_loss
 
     def forward(self, batch):
         return batch.sum((1, 2, 3))
@@ -49,16 +45,32 @@ class WeightSumModule(nn.Module):
     def __init__(self):
         super(WeightSumModule, self).__init__()
         self.name = 'wsum'
+        self.loss = mse_loss
         self.c = nn.Parameter(torch.Tensor([1.0]))
 
     def forward(self, x):
-        return (x * self.c).sum(((1, 2, 3)))
+        return (x * self.c).sum((1, 2, 3))
+
+
+class GTSumModule(nn.Module):
+    def __init__(self, device):
+        super(GTSumModule, self).__init__()
+        self.name = 'gt'
+        self.loss = mse_loss
+        self.threshold = nn.Parameter(torch.Tensor([1.0]))
+        self.one = torch.ones(1, requires_grad=True, device=device)
+        self.zero = torch.zeros(1, requires_grad=True, device=device)
+
+    def forward(self, x):
+        # return self.one if (x > self.threshold).all() else self.zero
+        return torch.where(x > self.threshold, self.one, self.zero)
 
 
 class FilterModule(nn.Module):
     def __init__(self):
         super(FilterModule, self).__init__()
         self.name = 'filter'
+        self.loss = mse_loss
         self.layers = nn.Sequential(
             nn.Conv2d(1, 1, (3, 3), padding=1, bias=True),
         )
@@ -67,58 +79,27 @@ class FilterModule(nn.Module):
         return self.layers(img_batch)
 
 
-@nf.nut_processor
-def Train(batches, network):
-    model = network.model
-    device = model.device
-    for inputs, outputs, fp in batches:
-        model.optimizer.zero_grad()
-        inputs = [torch.as_tensor(b, device=device).float() for b in inputs]
-        outputs = [torch.as_tensor(b, device=device).float() for b in outputs]
-        y_pred = network.model(inputs, fp)
-        y_true = outputs[0]
-        loss = model.losses(y_pred, y_true)
-        loss.backward()
-        model.optimizer.step()
-        yield loss.item()
-
-
-@nf.nut_processor
-def Predict(batches, network):
-    model = network.model
-    device = model.device
-    with torch.no_grad():
-        for inputs, fp in batches:
-            model.optimizer.zero_grad()
-            inputs = [torch.as_tensor(b, device=device).float() for b in inputs]
-            y_pred = network.model.forward(inputs, fp)
-            yield y_pred
-
-
 if __name__ == '__main__':
-    modules = [CountModule(), FilterModule(), WeightSumModule()]
-    model = NSModule(modules, device='cuda')
+    device = 'cuda'
+    modules = [CountModule(), FilterModule(), GTSumModule(device)]
+    model = NSModule(modules, device=device)
     model.optimizer = optim.Adam(model.parameters())
-    model.losses = mse_loss
-    network = PytorchNetwork(model, 'weights.pt')
 
     # add_channel = nf.MapCol(1, lambda img: np.expand_dims(img, 0))
 
     samples = sorted(gen_samples(), key=lambda s: s[2].count('('))
 
     print('training...')
-    for epoch in range(10):
+    for epoch in range(100):
         print('epoch', epoch)
-        losses = (samples >> BuildBatch(2) >>
-                  # Train(network) >>
-                  network.train() >>
-                  nf.Collect())
-        loss = np.mean(losses)
-        print('loss', loss)
-        if loss < 1e-6: break
+        t_losses = (samples >> BuildBatch(2) >>
+                    Train(model) >> nf.Collect())
+        print('t_loss', np.mean(t_losses))
+        v_losses = (gen_samples(10) >> BuildBatch(1) >>
+                    Validate(model) >> nf.Collect())
+        print('v_loss', np.mean(v_losses))
 
     print('predicting...')
-    (samples >> BuildBatch(2, outcol=None) >>
-     #Predict(network) >> nf.Flatten() >>
-     network.predict() >>
-     nf.Tail(3) >> nf.Print() >> nf.Consume())
+    round = nf.MapCol(1, lambda x: torch.round(x + 0.4))
+    (gen_samples(2) >> BuildBatch(1, outcol=None) >>
+     Predict(model) >> round >> nf.Print() >> nf.Consume())
